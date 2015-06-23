8b71c0eeeffc4f1298fe235f1cac7e101c4346bd
Cody Deckard <cjdeckard@gmail.com>
2015-06-23 23:10:05 +0000

    Added arguments to select functions

--- otp.rb
+++ otp.rb
@@ -6,29 +6,39 @@ if ARGV[0] == "g"
     seed = Random.new().seed
     f = File.open("seed.sec","w+")
     f.write(seed)
+    
+    elsif ARGV[0] == "e"
+        #Read seed from file
+        if File.exists?("seed.sec") 
+            seed = File.read("seed.sec").to_i
+        end
+
+        secret = Random.new(seed)
+        encoded = ""
+
+
+        "Hello".each_char do |c|
+            encoded = encoded << (c.ord ^ secret.rand(0..255))
+        end
+        puts encoded
+        
+    elsif ARGV[0] == "d"
+        secret = Random.new(seed)
+        decoded = ""
+        encoded.each_char do |c|
+            decoded = decoded <<(c.ord ^ secret.rand(0..255))
+        end
+
+        puts decoded
+    else
+    
+        puts "Please specify an option"
 end
 
-#Read seed from file
-if File.exists?("seed.sec") 
-    seed = File.read("seed.sec").to_i
-end
-
-secret = Random.new(seed)
-encoded = ""
 
 
-"Hello".each_char do |c|
-    encoded = encoded << (c.ord ^ secret.rand(0..255))
-end
-puts encoded
 
-#"Decoded"
 
-newSecret = Random.new(seed)
-decoded = ""
-encoded.each_char do |c|
-    decoded = decoded <<(c.ord ^ newSecret.rand(0..255))
-end
 
-puts decoded
+
 
