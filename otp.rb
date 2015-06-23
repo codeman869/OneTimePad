133c218a8819e9b8a7b898429b2832aa094f23ae
Cody Deckard <cjdeckard@gmail.com>
2015-06-23 21:37:37 +0000

    Add seed generation capability

--- otp.rb
+++ otp.rb
@@ -1,11 +1,19 @@
 #secret = Random.new()
 #seed = secret.seed
 
+#Generates a new seed
+if ARGV[0] == "g"
+    seed = Random.new().seed
+    f = File.open("seed.sec","w+")
+    f.write(seed)
+end
+
 #Read seed from file
 if File.exists?("seed.sec") 
-    seed = File.read("seed.sec")
+    seed = File.read("seed.sec").to_i
 end
-secret = Random.new(seed.to_i)
+
+secret = Random.new(seed)
 encoded = ""
 
 
@@ -16,7 +24,7 @@ puts encoded
 
 #"Decoded"
 
-newSecret = Random.new(seed.to_i)
+newSecret = Random.new(seed)
 decoded = ""
 encoded.each_char do |c|
     decoded = decoded <<(c.ord ^ newSecret.rand(0..255))
