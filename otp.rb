a33afd1c27407390f7c231d1bbc94640913e4033
Cody Deckard <cjdeckard@gmail.com>
2015-06-23 21:25:20 +0000

    Reading a seed from file

--- otp.rb
+++ otp.rb
@@ -1,6 +1,14 @@
-secret = Random.new()
-seed = secret.seed
+#secret = Random.new()
+#seed = secret.seed
+
+#Read seed from file
+if File.exists?("seed.sec") 
+    seed = File.read("seed.sec")
+end
+secret = Random.new(seed.to_i)
 encoded = ""
+
+
 "Hello".each_char do |c|
     encoded = encoded << (c.ord ^ secret.rand(0..255))
 end
@@ -8,7 +16,7 @@ puts encoded
 
 #"Decoded"
 
-newSecret = Random.new(seed)
+newSecret = Random.new(seed.to_i)
 decoded = ""
 encoded.each_char do |c|
     decoded = decoded <<(c.ord ^ newSecret.rand(0..255))
