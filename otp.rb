131a6507ea5c47c7c7ab21eff1c64e73fee96f43
Cody Deckard <cjdeckard@gmail.com>
2015-06-23 20:53:58 +0000

    Random seed usage

--- otp.rb
+++ otp.rb
@@ -1,4 +1,5 @@
-secret = Random.new(1234)
+secret = Random.new()
+seed = secret.seed
 encoded = ""
 "Hello".each_char do |c|
     encoded = encoded << (c.ord ^ secret.rand(0..255))
@@ -7,7 +8,7 @@ puts encoded
 
 #"Decoded"
 
-newSecret = Random.new(1234)
+newSecret = Random.new(seed)
 decoded = ""
 encoded.each_char do |c|
     decoded = decoded <<(c.ord ^ newSecret.rand(0..255))
