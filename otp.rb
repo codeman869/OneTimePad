f37ffe57a72d9dbb139a3ea633e347e508bdd45f
Cody Deckard <cjdeckard@gmail.com>
2015-06-24 02:20:49 +0000

    Piping capability

--- otp.rb
+++ otp.rb
@@ -1,17 +1,17 @@
-#secret = Random.new()
-#seed = secret.seed
+#!/usr/bin/env ruby
 
 #OTP Class for basic functionality
 class OTP
     def initialize(seed)
         @seed = seed
         @PRNG = Random.new(@seed)
+        
     end
     
     def encode(message)
         encoded = ""
         message.each_char do |c|
-            encoded = encoded << XOR(c)
+            encoded = encoded << (((c.ord ^ @PRNG.rand(32..126)) + 32) % 126)
         end
         encoded
     end
@@ -20,35 +20,32 @@ class OTP
     def decode(cypherText)
         decoded = ""
         cypherText.each_char do |c|
-            #puts "decoding #{c}"
-            decoded = decoded << XOR(c)
+            decoded = decoded << (((c.ord - 32) % 126) ^ @PRNG.rand(32..126))
         end
         decoded
     end
     
-    private
-    
-    def XOR(m)
-        m.ord ^ @PRNG.rand(0..255)
-    end
 end
 
+options = ARGV.shift
+input_stream = $stdin.read
+#puts input_stream
 
 if ARGV[0] == "g"
     seed = Random.new().seed
     f = File.open("seed.sec","w+")
     f.write(seed)
     f.close
-elsif ARGV[0] == "e" || ARGV[0] == "d"
+elsif options == "e" || options == "d"
     #Read seed from file
     if File.exists?("seed.sec") 
         seed = File.read("seed.sec").to_i
     end
-    message = ARGV[1]
+    message = ARGV[1] || input_stream
     enc_dec = OTP.new(seed)
         
-    puts enc_dec.encode(message) if ARGV[0] == "e"
-    puts enc_dec.decode(message) if ARGV[0] == "d"
+    puts enc_dec.encode(message) if options == "e"
+    puts enc_dec.decode(message) if options == "d"
         
 else
     
